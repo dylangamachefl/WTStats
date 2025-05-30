da2693070799a8c2dadd74382a0ff7e657d4d826
Dylan Gamache <dylangamachefl@gmail.com>
2025-05-30 19:48:33 +0000

    update base

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  basePath: isProd ? '' : '',
+  basePath: isProd ? '/WTStats' : '',
   images: {
     remotePatterns: [
       {
